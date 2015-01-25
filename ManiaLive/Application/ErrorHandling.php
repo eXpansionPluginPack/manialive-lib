831b7b98d776bfee30d622192694a1e8618c7fd5
oliverde8 <oliverde8@gmail.com>
2015-01-25 10:58:19 +0100

    Added support for error reporting²

--- ManiaLive/Application/ErrorHandling.php
+++ ManiaLive/Application/ErrorHandling.php
@@ -62,6 +62,7 @@ abstract class ErrorHandling
 	 */
 	public static function processRuntimeException(\Exception $e)
 	{
+		self::logError($e);
 		self::displayAndLogError($e);
 		self::increaseErrorCount();
 	}
